813289209e164bc6e87ac81a7fb3691d061cec0b
seoyeon <dltjdus0725@naver.com>
2025-01-14 23:29:57 +0900

    250114 saveFiles

--- src/main/java/org/zerock/apiserver/util/CustomFileUtil.java
+++ src/main/java/org/zerock/apiserver/util/CustomFileUtil.java
@@ -8,7 +8,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @Log4j2
@@ -34,6 +39,24 @@ public class CustomFileUtil {
     }
 
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
-       return null;
+
+        if(files == null || files.size() == 0) {
+//          return List.of();
+            return null;
+        }
+
+        List<String> uploadNames = new ArrayList<>();
+
+        for(MultipartFile file: files) {
+
+            //uuid 활용하여 처리
+            String savedName = UUID.randomUUID().toString()+"_"+file.getOriginalFilename();
+
+            Path savePath = Paths.get(uploadPath, savedName);
+
+        }
+
+
+       return uploadNames;
     }
 }
